d63968ec97419f5b173a225c2c4dc6ed6716d5f3
tarikselimovic <selimovic.t@gmail.com>
2024-04-14 00:24:47 +0200

    dd

---
+++ TarikTestApp/Program.cs
@@ -0,0 +1,39 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//var port = "5000";
+//var url = $"http://0.0.0.0:{port}";
+//app.Urls.Add(url);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000";
+
+
+//app.Urls.Add(urls); // Add the URLs to listen on
+
+app.Run();
+
+//app.Run();
+
